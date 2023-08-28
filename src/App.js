7f5297eee7d3f19ccd95ed11af397c1a15c0d3d2
Ксения Пастухова <ksenialukina@MacBook-Pro-Ksenia.local>
2023-08-28 16:54:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    fetch("https://www.floatrates.com/daily/usd.json")
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию");
+      });
+  }, []);
+  const onChangeFromPrice = (value) => {
+    const selectedCurrency = ratesRef.current[fromCurrency.toLowerCase()].rate;
+    const price = value / selectedCurrency;
+    const targetCurrency = ratesRef.current[toCurrency.toLowerCase()].rate;
+    const result = price * targetCurrency;
+    setFromPrice(value);
+    setToPrice(result.toFixed(2));
+  };
+  const onChangeToPrice = (value) => {
+    const selectedCurrency = ratesRef.current[fromCurrency.toLowerCase()];
+    const targetCurrency = ratesRef.current[toCurrency.toLowerCase()];
+    const result = (selectedCurrency.rate / targetCurrency.rate) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  };
+  useEffect(() => {
+    if (Object.keys(ratesRef.current).length > 0) {
+      onChangeFromPrice(fromPrice);
+    }
+  }, [fromPrice, fromCurrency, ratesRef.current]);
+
+  useEffect(() => {
+    if (Object.keys(ratesRef.current).length > 0) {
+      onChangeToPrice(toPrice);
+    }
+  }, [toPrice, toCurrency, ratesRef.current]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
